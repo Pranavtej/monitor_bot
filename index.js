dd0ccb42719bb6be5b2652d0df0e782a53647d79
Pranavtej <pranav_mcr@srkrec.edu.in>
2024-01-10 17:07:02 +0530

    fix: changes

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const bot = new TelegramBot(botToken, { polling: true });
 
 const sendMessage = (message) => {
     bot.sendMessage(chatId, message);
+    bot.launch().then(() => {   
+        bot.sendMessage(chatId, message);   
+    });
 };
 
 
@@ -21,16 +24,18 @@ const monitorAPI = async () => {
         const response = await axios.get(apiEndpoint);
         console.log(response.data.data.descendants);
         console.log('API is working fine')
+        const errorMessage = `⚠️⚠️Warning Desecndants: ${response.data.data.descendants}`;
+        sendMessage(errorMessage);
         
-        if (response.data.data.descendants >13 && response.data.data.descendants < 18) {
+        // if (response.data.data.descendants >13 && response.data.data.descendants < 18) {
            
-            const errorMessage = `⚠️⚠️Warning Desecndants: ${response.data.data.descendants}`;
-            sendMessage(errorMessage);
-        }
-        else if  (response.data.data.descendants >= 18) {
-            const errorMessage = ` Broo !!!❌❌❌ ${response.data.data.descendants}`;
-            sendMessage(errorMessage);
-        }
+        //     const errorMessage = `⚠️⚠️Warning Desecndants: ${response.data.data.descendants}`;
+        //     sendMessage(errorMessage);
+        // }
+        // else if  (response.data.data.descendants >= 18) {
+        //     const errorMessage = ` Broo !!!❌❌❌ ${response.data.data.descendants}`;
+        //     sendMessage(errorMessage);
+        // }
     } catch (error) {
         sendMessage(`API failed with exception: ${error}`);
     }
